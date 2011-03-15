0f3472dd739adda269785238d9e7cb51181bbaed
Kevin Risden <compuwizard123@gmail.com>
2011-03-15 14:15:11 -0400

    Add base Lab3 Visual Studio project files

---
+++ ExpediaTest/FlightTest.cs
@@ -0,0 +1,12 @@
+using System;
+using Expedia;
+using NUnit.Framework;
+
+namespace ExpediaTest
+{
+	[TestFixture()]
+	public class FlightTest
+	{
+		//TODO Task 7 items go here
+	}
+}
---
+++ ExpediaTest/HotelTest.cs
@@ -0,0 +1,12 @@
+using System;
+using Expedia;
+using NUnit.Framework;
+
+namespace ExpediaTest
+{
+    [TestFixture()]
+    public class HotelTest
+    {
+		//TODO Task 6 items go here
+    }
+}
